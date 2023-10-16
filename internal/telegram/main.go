a6159b0e5fda11ac26f7dc7d371ef91348adf9b9
Qesait <kuzva2003@gmail.com>
2023-10-16 18:34:14 +0300

    telegram commands update

---
+++ internal/telegram/main.go
@@ -0,0 +1,118 @@
+package telegram
+
+import (
+	"TeleBotNotifications/internal/config"
+	"TeleBotNotifications/internal/logger"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var apiURL = "https://api.telegram.org"
+
+type Bot struct {
+	token       string
+	commands    []command
+	http_client *http.Client
+	updateDelay time.Duration
+	adminChatId int
+	lastUpdate  int
+}
+
+func NewBot(config *config.TelegramConfig) Bot {
+	return Bot{
+		token:       config.BotToken,
+		http_client: &http.Client{},
+		updateDelay: time.Duration(config.UpdateDelay) * time.Second,
+		adminChatId: config.AdminChatId,
+		lastUpdate:  0,
+	}
+}
+
+type Message struct {
+	updateId int
+	UserId   int
+	ChatId   int
+	Text     string
+}
+
+func (b *Bot) Run(port uint) {
+	err := b.UpdateCommands()
+	if err != nil {
+		logger.Error.Println(err)
+	}
+
+	logger.General.Println("Telegram bot started")
+
+	for {
+		time.Sleep(b.updateDelay)
+		messages, err := b.getNewMessages()
+		if err != nil {
+			logger.Error.Println("Error getting new messages: ", err)
+			continue
+		}
+		if len(messages) == 0 {
+			continue
+		}
+
+		for i := 0; i < len(messages); i++ {
+			if messages[i].updateId > b.lastUpdate {
+				b.lastUpdate = messages[i].updateId
+			}
+			if !strings.HasPrefix(messages[i].Text, "/") {
+				continue
+			}
+			b.handleCommand(messages[i])
+		}
+
+	}
+}
+
+func (b *Bot) handleCommand(message Message) {
+	for j := 0; j < len(b.commands); j++ {
+		if strings.HasPrefix(message.Text, b.commands[j].Keyword) {
+			logger.General.Println("<-" + message.Text)
+			message.Text = strings.TrimSpace(strings.TrimPrefix(message.Text, b.commands[j].Keyword))
+			b.commands[j].Handler(message)
+			return
+		}
+	}
+}
+
+func (b *Bot) SendMessage(message string, to int) error {
+	resource := fmt.Sprintf("/bot%s/sendMessage", b.token)
+	params := url.Values{
+		"chat_id": {strconv.Itoa(to)},
+		"text":    {message},
+	}
+
+	u, _ := url.ParseRequestURI(apiURL)
+	u.Path = resource
+	u.RawQuery = params.Encode()
+	requestUrl := u.String()
+
+	response, err := http.Get(requestUrl)
+	if err != nil {
+		logger.Error.Println("telegram bot failed to send message with error", err)
+		return fmt.Errorf("sending request failed with err: %s", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		logger.Error.Println("telegram bot failed to send message because of unexpected status code", response)
+		return fmt.Errorf("unexpected status code: %s", response.Status)
+	}
+
+	return nil
+}
+
+func (b *Bot) Write(p []byte) (n int, err error) {
+	err = b.SendMessage(string(p), b.adminChatId)
+	if err != nil {
+		return 0, err
+	}
+	return len(p), nil
+}
---
+++ internal/telegram/updates.go
@@ -0,0 +1,84 @@
+package telegram
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+type update struct {
+	UpdateId int `json:"update_id"`
+	Message  struct {
+		MessageId int `json:"message_id"`
+		From      struct {
+			Id           int    `json:"id"`
+			IsBot        bool   `json:"is_bot"`
+			FirstName    string `json:"first_name"`
+			Username     string `json:"username"`
+			LanguageCode string `json:"language_code"`
+		} `json:"from"`
+		Chat struct {
+			Id        int    `json:"id"`
+			FirstName string `json:"first_name"`
+			Username  string `json:"username"`
+			Type      string `json:"type"`
+		} `json:"chat"`
+		Date int    `json:"date"`
+		Text string `json:"text"`
+	} `json:"message"`
+}
+
+type updateResponse struct {
+	Ok     bool     `json:"ok"`
+	Result []update `json:"result"`
+}
+
+func extract(update_with_message update) Message {
+	return Message{
+		updateId: update_with_message.UpdateId,
+		UserId:   update_with_message.Message.From.Id,
+		ChatId:   update_with_message.Message.Chat.Id,
+		Text:     update_with_message.Message.Text,
+	}
+}
+
+func (b *Bot) createUpdateUrl() string {
+	resource := fmt.Sprintf("/bot%s/getUpdates", b.token)
+	params := url.Values{
+		"offset":          {strconv.Itoa(b.lastUpdate + 1)},
+		"allowed_updates": {"[\"message\"]"},
+	}
+
+	u, _ := url.ParseRequestURI(apiURL)
+	u.Path = resource
+	u.RawQuery = params.Encode()
+	return u.String()
+}
+
+func (b *Bot) getNewMessages() ([]Message, error) {
+	requestUrl := b.createUpdateUrl()
+	response, err := b.http_client.Get(requestUrl)
+	if err != nil {
+		return nil, fmt.Errorf("sending request failed with err: %s", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %s", response.Status)
+	}
+
+	updates := &updateResponse{}
+	err = json.NewDecoder(response.Body).Decode(updates)
+	if err != nil {
+		return nil, fmt.Errorf("decoding failed with error: %s", err)
+	}
+
+	messages := make([]Message, 0, len(updates.Result))
+	for i := 0; i < len(updates.Result); i++ {
+		messages = append(messages, extract(updates.Result[i]))
+	}
+
+	return messages, nil
+}
