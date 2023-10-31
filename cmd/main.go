bcdfdaffe6ef2010f404aba67318af23f80220ef
Qesait <kuzva2003@gmail.com>
2023-10-31 16:47:09 +0300

    time compare fix

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"TeleBotNotifications/internal/app"
+	"time"
 )
 
-
 func main() {
+	loc, _ := time.LoadLocation("UTC")
+    time.Local = loc
+
 	server, err := app.New()
 	if err != nil {
 		panic(err)
