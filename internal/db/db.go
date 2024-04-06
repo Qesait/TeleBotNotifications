07622fdd6222a2f27bf8f07348ab65c09398c4a8
Qesait <kuzva2003@gmail.com>
2024-04-06 18:15:36 +0300

    safe db get, set

--- internal/db/db.go
+++ internal/db/db.go
@@ -10,13 +10,17 @@ import (
 	"sync"
 )
 
-type DB struct {
+type User struct {
 	UserId    int                 `json:"user_id"`
 	ChatId    int                 `json:"chat_id"`
 	Token     spotify.OAuth2Token `json:"token"`
 	LastCheck string              `json:"last_check"`
-	saveFile  string
-	mu        sync.Mutex
+}
+
+type DB struct {
+	user     *User
+	saveFile string
+	mu       sync.Mutex
 }
 
 func NewDB(saveFile string) DB {
@@ -26,41 +30,66 @@ func NewDB(saveFile string) DB {
 func (db *DB) Load() error {
 	db.mu.Lock()
 	defer db.mu.Unlock()
+
 	jsonFile, err := os.Open(db.saveFile)
 	if err != nil {
-		return fmt.Errorf("db load | can't open save file: %w", err)
+		return fmt.Errorf("can't open save file: %w", err)
 	}
 	defer jsonFile.Close()
 
 	byteValue, err := io.ReadAll(jsonFile)
 	if err != nil {
-		return fmt.Errorf("db load | can't read save file: %w", err)
+		return fmt.Errorf("can't read save file: %w", err)
 	}
-	err = json.Unmarshal(byteValue, &db)
+	user := &User{}
+	err = json.Unmarshal(byteValue, user)
 	if err != nil {
-		return fmt.Errorf("db load | wrong save file format: %w", err)
+		return fmt.Errorf("wrong save file format: %w", err)
 	}
+	db.user = user
 	logger.General.Println("db loaded")
 	return nil
 }
 
-func (db *DB) save() error {
+func (db *DB) Save() error {
+	if db.user == nil {
+		return nil
+	}
 	db.mu.Lock()
 	defer db.mu.Unlock()
+
 	jsonFile, err := os.Create(db.saveFile)
 	if err != nil {
-		return fmt.Errorf("db save | can't open save file: %w", err)
+		return fmt.Errorf("can't open save file: %w", err)
 	}
 	defer jsonFile.Close()
 
-	byteValue, err := json.MarshalIndent(db, "", "    ")
+	byteValue, err := json.MarshalIndent(db.user, "", "    ")
 	if err != nil {
-		return fmt.Errorf("db save | can't marshal save data: %w", err)
+		return fmt.Errorf("can't marshal save data: %w", err)
 	}
 	_, err = jsonFile.Write(byteValue)
 	if err != nil {
-		return fmt.Errorf("db save | can't write into save file: %w", err)
+		return fmt.Errorf("can't write into save file: %w", err)
 	}
 	logger.General.Println("db saved")
 	return nil
+}
+
+func (db *DB) Set(newUser User) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	db.user = &newUser
+}
+
+func (db *DB) Get() *User {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	if db.user == nil {
+		return nil
+	}
+	userCopy := *db.user
+	return &userCopy
 }
